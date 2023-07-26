1294e978da0beef49127b6d4cbdb534051a4820a
Sched71 <scheka.dm@gmail.com>
2023-07-26 10:06:11 +0300

    Add Sorted partitions creation

--- src/algorithms/order/order.h
+++ src/algorithms/order/order.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #include <memory>
+#include <unordered_map>
+
+#include <boost/container_hash/hash.hpp>
 
 #include "algorithms/algorithm.h"
 #include "model/column_layout_typed_relation_data.h"
+#include "sorted_partitions.h"
 #include "util/config/tabular_data/input_table_type.h"
 
 namespace algos::order {
@@ -11,13 +15,17 @@ namespace algos::order {
 class Order : public Algorithm {
 private:
     using TypedRelation = model::ColumnLayoutTypedRelationData;
+    using SortedPartitions = std::unordered_map<std::vector<unsigned int>, SortedPartition,
+                                                boost::hash<std::vector<unsigned int>>>;
 
     util::config::InputTable input_table_;
     std::unique_ptr<TypedRelation> typed_relation_;
+    SortedPartitions sorted_partitions_;
 
     void RegisterOptions();
     void LoadDataInternal() override;
     void ResetState() override;
+    void CreateSortedPartitions();
     unsigned long long ExecuteInternal() final;
 
 public:
