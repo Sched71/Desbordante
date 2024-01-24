e7bddbe490ed647d8d80aa528924c4a308d3458f
Michael Polyntsov <Arno9148@gmail.com>
2024-01-25 00:07:55 +0300

    Use boost::unordered_flat_set to represent EquivalenceClass

--- src/core/algorithms/od/order/sorted_partitions.h
+++ src/core/algorithms/od/order/sorted_partitions.h
@@ -4,6 +4,7 @@
 #include <unordered_set>
 #include <vector>
 
+#include <boost/unordered/unordered_flat_set.hpp>
 #include <boost/unordered/unordered_flat_map.hpp>
 
 #include <boost/unordered/unordered_flat_map.hpp>
@@ -16,7 +17,7 @@ namespace algos::order {
 
 class SortedPartition {
 public:
-    using EquivalenceClass = std::unordered_set<model::TupleIndex>;
+    using EquivalenceClass = boost::unordered_flat_set<model::TupleIndex>;
     using EquivalenceClasses = std::vector<EquivalenceClass>;
     using PartitionIndex = unsigned long;
     using HashProduct = boost::unordered_flat_map<PartitionIndex, SortedPartition::EquivalenceClasses>;
