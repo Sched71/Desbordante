6f154c5821a6cc60c48587e0935b511e9754a8e8
Sched71 <scheka.dm@gmail.com>
2024-02-02 17:39:49 +0300

    Add main for expirements

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -6,20 +6,50 @@
 
 INITIALIZE_EASYLOGGINGPP
 
-int main(int argc, char *argv[]) {
-    el::Loggers::configureFromGlobal("logging.conf");
-
-    if (argc != 2) {
-        std::cout << "Usage:: " << argv[0] << " path\n";
-        return 1;
-    }
-
+unsigned long long Epic() {
     algos::order::Order order;
-    config::InputTable parser = std::make_shared<CSVParser>(argv[1], '|', true);
-
+    config::InputTable parser = std::make_shared<CSVParser>("input_data/OD/EpicMeds.csv", '|', true);
     order.SetOption("table", std::move(parser));
     order.LoadData();
     unsigned long long time = order.Execute();
-    std::cout << "Order deps num: " << order.GetValidODs().size() << ", time: " << time << '\n';
-    return 0;
+    std::cout << "Epic time: " << time << '\n';
+    return time;
+}
+
+unsigned long long Modis() {
+    algos::order::Order order;
+    config::InputTable parser = std::make_shared<CSVParser>("input_data/OD/modis_2000-2019_Australia.csv", ',', true);
+    order.SetOption("table", std::move(parser));
+    order.LoadData();
+    unsigned long long time = order.Execute();
+    std::cout << "Modis time: " << time << '\n';
+    return time;
+}
+
+unsigned long long Bay() {
+    algos::order::Order order;
+    config::InputTable parser = std::make_shared<CSVParser>("input_data/OD/bay_wheels_data_wrangled.csv", ',', true);
+    order.SetOption("table", std::move(parser));
+    order.LoadData();
+    unsigned long long time = order.Execute();
+    std::cout << "Bay time: " << time << '\n';
+    return time;
+}
+
+unsigned long long Legacy() {
+    algos::order::Order order;
+    config::InputTable parser = std::make_shared<CSVParser>("input_data/OD/LegacyAlt.csv", ',', true);
+    order.SetOption("table", std::move(parser));
+    order.LoadData();
+    unsigned long long time = order.Execute();
+    std::cout << "Legacy time: " << time << '\n';
+    return time;
+}
+
+int main(int argc, char *argv[]) {
+    el::Loggers::configureFromGlobal("logging.conf");
+    Epic();
+    Modis();
+    Bay();
+    Legacy();
 }
