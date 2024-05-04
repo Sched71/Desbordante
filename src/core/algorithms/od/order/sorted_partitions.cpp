8e99bf8c26d6b7c83f64da32554596ce068b5fad
Sched71 <scheka.dm@gmail.com>
2024-05-04 18:12:25 +0300

    Add parallel partition product to Order

--- src/core/algorithms/od/order/sorted_partitions.cpp
+++ src/core/algorithms/od/order/sorted_partitions.cpp
@@ -1,7 +1,13 @@
 #include "sorted_partitions.h"
 
+#include <future>
 #include <unordered_map>
+#include <iostream>
 #include <vector>
+
+#include <boost/asio/post.hpp>
+#include <boost/asio/thread_pool.hpp>
+#include <boost/thread.hpp>
 
 #include "model/table/tuple_index.h"
 
@@ -25,9 +31,9 @@ SortedPartition::HashProduct SortedPartition::BuildHashProduct(
     HashProduct hash_product;
     hash_product.reserve(hash_partition_.size());
     for (auto it = begin; it != end; ++it) {
-        if (end - begin <= 1) {
+        /*if (end - begin <= 1) {
             break;
-        }
+        }*/
         std::unordered_set<PartitionIndex> visited_positions;
         for (model::TupleIndex tuple_index : *it) {
             auto it = hash_partition_.find(tuple_index);
@@ -65,6 +71,84 @@ void SortedPartition::Intersect(SortedPartition const& other) {
             }
         }
     }
+    res.sorted_partition_.shrink_to_fit();
+    *this = std::move(res);
+}
+
+void PrintSortedPartitions(SortedPartition const& p) {
+    for (SortedPartition::EquivalenceClass eq_class : p.GetEqClasses()) {
+        std::cout << "{"; 
+        for (auto it = eq_class.begin(); it != eq_class.end(); ++ it) {
+            std::cout << *it;
+            if (std::next(it) != eq_class.end()) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << "} ";
+    }
+    std::cout << '\n';
+}
+
+void SortedPartition::IntersectParallel(SortedPartition const& other,
+                                        config::ThreadNumType thread_num) {
+    BuildHashTable();
+
+    boost::asio::thread_pool pool(thread_num);
+    std::vector<std::future<HashProduct>> futures;
+    unsigned long chunk_size = num_rows_ / thread_num + 1;
+    unsigned long current_chunk_size = 0;
+    auto chunk_begin = other.sorted_partition_.begin();
+    for (auto eq_class_it = other.sorted_partition_.begin();
+         eq_class_it != other.sorted_partition_.end(); ++eq_class_it) {
+        current_chunk_size += eq_class_it->size();
+        if (current_chunk_size > chunk_size){
+            //std::cout << eq_class_it - other.sorted_partition_.begin() << '\n';
+            current_chunk_size = 0;
+            std::packaged_task<HashProduct()> task([this, chunk_begin, eq_class_it]() {
+                return BuildHashProduct(chunk_begin, eq_class_it);
+            });
+            futures.push_back(task.get_future());
+            boost::asio::post(pool, std::move(task));
+            chunk_begin = eq_class_it;
+        }
+    }
+    auto end = other.sorted_partition_.end();
+    std::packaged_task<HashProduct()> task([this, chunk_begin, end]() {
+        return BuildHashProduct(chunk_begin, end);
+    });
+    futures.push_back(task.get_future());
+    boost::asio::post(pool, std::move(task));
+
+    pool.join();
+
+    std::vector<HashProduct> results;
+    for (auto& future : futures) {
+        results.push_back(future.get());
+    }
+
+    SortedPartition res(num_rows_);
+    res.sorted_partition_.reserve(num_rows_);
+    for (std::size_t i = 0; i < sorted_partition_.size(); ++i) {
+        if (EquivalenceClass const& eq_class = sorted_partition_[i]; eq_class.size() == 1) {
+            res.sorted_partition_.push_back(eq_class);
+        } else {
+            for (HashProduct& hash_product : results){
+                auto eq_classes_it = hash_product.find(i);
+                if (eq_classes_it == hash_product.end()) {
+                    continue;
+                }
+                for (EquivalenceClass& eq_class : eq_classes_it->second) {
+                    if (!eq_class.empty()) {
+                        res.sorted_partition_.push_back(std::move(eq_class));
+                    }
+                }
+            }
+        }
+    }
+    //std::cout << '\n';
+    //PrintSortedPartitions(*this);
+    //PrintSortedPartitions(other);
+    //PrintSortedPartitions(res);
     res.sorted_partition_.shrink_to_fit();
     *this = std::move(res);
 }
